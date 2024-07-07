a8082c3689b2f43ae578f8c938727d2feb6b5768
AyeshaSAM44 <a747430@gmail.com>
2024-07-07 18:13:05 +0500

    assignment

---
+++ index.js
@@ -0,0 +1,166 @@
+//                       multiplication table  1 problem done 
+
+
+
+
+
+// let userInput = prompt("Please enter no");
+// for (let i = 1; i <= 1; i++) {
+//       for ( let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} =  ${i * j} `);
+//       }
+//     }
+    
+//                                  2 problem  done
+
+
+// let numbers = [];
+// for (let i = 1; i <= 5; i++) {
+//     let num = +prompt("enter five no  mam");
+//     numbers.push(num);
+// }
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+// console.log(`The sum of the array elements is: ${sum}`);
+
+
+
+//                                         3 problem 
+// let numberzz = [];
+// let maxNum = []
+// for (let i = 0; i <= 5; i++) {
+//     let num = prompt("enter five no sir ");
+//     numberzz.push(num);
+// }
+// for (let i = 1; i < numberzz.length; i++) {
+//     if (numberzz[i] > maxNum) {
+      
+//     }
+// }
+
+
+// problem 4
+
+// let userInput = prompt("please enter a string ")
+// for (var i = 1; i <= 3; i--) {
+
+  
+// }
+
+
+
+// prolem 5
+// let userInput = +prompt("enetr a no");
+// for (var i = 1; i <= 4; i++) {
+//     console.log(userInput);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  first problem solvedddddddddddd
+
+// let userArray = [];
+
+// for (let i = 0; i < 5; i++) {
+//     let userInput = +prompt("sir enter  a no");
+//     userArray.push(userInput);
+// }
+// let smallestNumber = userArray[0];
+
+//                       for (let i = 1; i < userArray.length; i++) {
+//                       if (userArray[i] < smallestNumber) {
+//                      smallestNumber = userArray[i];
+//                      }
+//                   }
+//  console.log("The smallest number in the array i " + smallestNumber);
+
+
+// 2 problem solvedddddddddd
+
+// let name = "ayesha";
+// let reversedName = "";
+// for (let i = name.length - 1; i >= 0; i--) {
+//     reversedName += name[i];
+// }
+// console.log("seedha naam = " + name);
+// console.log("ulta naam = " + reversedName);
+
+// problem 3 solveddddddddddddddddddddd
+
+// let numbers = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+// if (numbers.length > 0) {
+//     average = sum / numbers.length;
+// }
+// console.log("The average of the array is: " + average);
+
+// average means total ayesha 10+20+30+40+50
+
+
+
+
+// problem 4
+// let celsius = 30;
+// let fahrenheit = (celsius * 9/5) + 32;
+// console.log(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
+
+
+
+// problem 5
+
+// Convert Fahrenheit to Celsius
+let fahrenheit = 68; 
+let celsius = (fahrenheit - 32) * 5/9;
+console.log(fahrenheit + " degrees Fahrenheit is equal to " + celsius + " degrees Celsius.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
